675ab4d8ed2ede4371f247401a728574ac48884e
Irvingm07 <58572859+Irvingm07@users.noreply.github.com>
2019-12-11 12:15:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,71 +2,78 @@
 <html lang="es-mx">
 <head>
     <meta charset="UTF-8">
-    <title>IntroPhp7 Grupo XA</title>
-    <link rel="stylesheet" href="css/estilos.css"/>
-    <link rel="stylesheet" href="css/menu.css"/>
-    <link rel="stylesheet" href="css/problema.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Science Technology Engineerig & Mathematics</title>
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="css/estilos.css">
     <link href="https://fonts.googleapis.com/css?family=Rambla" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
-</head>
-<body>
-   <section class="wrapper">
-    <header>
-      <h1 class="logo"><a href="index.php">IntroPHP7</a></h1>
-      <nav>
-      <ul>
-        <li><a href="index.php" class="current">Inicio</a></li>
-        <li><a href="#">Otros Ejemplos Php7</a></li>
-      </ul>
-      </nav>
+    <script>
+         function solucion1(){
+        var masa=0.06;
+        var vt = 81;
+        var radio = 1.5;
+        var fc = masa * vt / radio;
+        var a=Math.round(fc);
+        var d=document.getElementById("resultado");
+        d.innerHTML='<h2>Resultado: </h2> Ejercicio 1:<br></br> a)La fuerza centrípeta de la piedra es: <br></br> Fc= '+ fc +'  N' ;
+        }
+    </script>
+ </head>
+ <body>
+    <section id="contenedor">
+    <header id="gridHeader">
+    <h1>Science Technology Engineerig & Mathematics</h1>
+    <h2>Resolver problemas de Ciencia e Ingeniería con JS</h2>
+    <h2>Nombre: Irving René Medina Soto No. de control: 17091103</h2>
+    
     </header>
-<section id="contenedor">
-<section  class="problema">
-  <h2>Problema: Cálcular la densidad de la Tierra</h2>
-  <p>Descripción:</p>
-  <p>El radio de la Tierra es 6 378 km y su masa es 5.97E24 kg. <br>
-a) ¿Cuál es la densidad promedio de la Tierra?<br>
-b) Si el radio de la Tierra se redujera a una tercera parte y su masa no cambiará, cuál serı́a su densidad?<br>
-c) Si el radio de la Tierra y su masa se redujera a una
-tercera parte, ¿su densidad serı́a la misma? ¿por qué?</p>
-</section>
-<section class="formulas">
-<h2>Fórmulas</h2>
-    Densidad = Masa/Volumen<br>
-    Volumen=(4/3)pi*r*r*r
-</section>
-<section class="datos">
-<h2>Datos:</h2>
-   m= 5.97E24 Kg. <br>
-   r= 6378 Km.
-</section>
-<section class="calculos">
-<h2>Solución</h2>
-   <p>a)La densidad de la Tierra es:<br>
-    densidad = 5.97E24 Kg/volumen (m) <br>
-    volumen= (4/3)*pi*(6378000)*(6378000)*(6378000)</p>
-</section>
-<?php
-     function calcula_densidad(){
-        $PI=3.141592;
-        $masa= 5.97E24;
-        $radio= 6378e3;
-        $volumen= (4/3)*$PI*$radio*$radio*$radio;
-        $densidad= $masa/$volumen;
-        return $densidad;
-     }
-?>
-<section class="resultado">
-<h2>Resultado:</h2>
-<div id="resultadoA"></div>
-<?php
- print "<h1> resultado: densidad = ".calcula_densidad(). " kg/(metro cubico)</h1>";
-?>
-</section>
-</section>
-    <footer class="pie">
-     Creative Commons versión 3.0 SciSoft 2019
-    </footer>
-   </section>
-</body>
-</html>
+    </section>
+    <section id="problema"  class="gridProblema">
+     <section id=descripcion>
+     <h2><center>Problema:</center></h2>
+     <p>1. Una piedra de 0.6kg de masa se hace girar mediante una cuerda de 1.5 metros de longitud. si ésta presenta
+        en su superficie una velocidad tangencial de 9m/s. ¿cuál es su fuerza centrípeta?
+     </p>
+            <p>a) Calcule la fuerza centrípeta de la piedra<br>
+             
+               
+      </section>
+      <section id="datos">
+            <h2>Datos:</h2>
+            Masa = 0.06kg <br>
+            velocidad tangencial = 9 m/s<br>
+            radio = 1.5 m<br>
+            
+            <br>
+      </section>
+      <section id="formulas">
+            <h2>Fórmulas:</h2>
+            w = θ / t<br>
+            Fc = (m)(vt)^2/r 
+           
+            <br>
+      </section>
+      <section id="solucion">
+            <h2>Solución: </h2>
+            Ejercicio 1:
+            <p> a) Calcule la fuerza centrípeta de la piedra.<br>
+                <br>
+             Fc = (0.06kg)(9m/s)^2/1.5m <br>
+             <br>
+             <button onclick="solucion1()">Presiona para calcular</button><br>
+             <br>
+      </section>
+      <section id="resultado">
+      <h2>Resultado:</h2>
+      Ejercicio 1: 
+      <p> a) La fuerza centrípeta de la piedra es:<br>     
+         <br>
+     </section>
+    </section>
+     <footer id="gridPie">
+        <p>Solution Science Problems. 2019 Creative Commons 3.0</p>
+     </footer>
+ </body>
+ </html>
